eed4b8557bac0e768671ff190ac37a734639d6a6
Max Tellhed <max.tellhed@gmail.com>
2022-07-30 11:36:18 +0200

    deleted text

--- learningJavaScript/index.js
+++ learningJavaScript/index.js
@@ -2,10 +2,5 @@
 showPrimes(10);
 
 function showPrimes(limit) {
-    console.log("Haha")
-    console.log("Walla Walla Wooooo")
-    let die = "String"
-    console.log("im on the other computer")
-    console.log("Hi computer2")
-
+    
 }
